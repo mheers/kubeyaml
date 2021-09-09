110b53f5a01c53620dfd74813438ca9867accf7f
jayme-github <jayme-github@users.noreply.github.com>
2021-09-09 10:56:06 +0200

    Fix TestSortVersionsNoErrors
    SortVersions docstring sais: sorts a list of versions from newest to oldest
    Reflect that in the test.

--- backend/internal/versions_test.go
+++ backend/internal/versions_test.go
@@ -14,7 +14,7 @@ func TestSortVersionsNoErrors(t *testing.T) {
 		{
 			name: "simple test",
 			args: []string{"1.15", "1.17", "1.18", "1.16"},
-			want: []string{"1.15", "1.16", "1.17", "1.18"},
+			want: []string{"1.18", "1.17", "1.16", "1.15"},
 		},
 	}
 	for _, tt := range tests {
