5a2f13f4ef4e2ddf073a3cb17eada762c5714194
Marcel Heers <info@heers.it>
2022-08-31 13:16:45 +0300

    fixes

--- internal/kubernetes/data/lookup.go
+++ internal/kubernetes/data/lookup.go
@@ -31,6 +31,18 @@ func (s *StaticFiles) Swagger(version string) []byte {
 		return s.OneEighteen()
 	case "1.19":
 		return s.OneNineteen()
+	case "1.20":
+		return s.OneTwenty()
+	case "1.21":
+		return s.OneTwentyOne()
+	case "1.22":
+		return s.OneTwentyTwo()
+	case "1.23":
+		return s.OneTwentyThree()
+	case "1.24":
+		return s.OneTwentyFour()
+	case "1.25":
+		return s.OneTwentyFive()
 	default:
 		panic(fmt.Sprintf("unknown version %v", version))
 	}
